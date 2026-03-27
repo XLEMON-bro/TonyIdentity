96a5e901268bc068d7b14d8a62932001c081e00e
Vlad <vladavramchuk@gmail.com>
2026-03-27 21:25:32 +0200

    Fixed issues with table names, added nuget package for migrations, created init migration

--- TonyIdentity.Database/Context/TonyIdentityDbContext.cs
+++ TonyIdentity.Database/Context/TonyIdentityDbContext.cs
@@ -32,7 +32,7 @@ namespace TonyIdentity.Database.Context
             builder.Entity<ApplicationUser>().ToTable("Users");
             builder.Entity<IdentityRole<Guid>>().ToTable("Roles");
             builder.Entity<IdentityUserRole<Guid>>().ToTable("UserRoles");
-            builder.Entity<IdentityUserClaim<Guid>>().ToTable("UserRoles");
+            builder.Entity<IdentityUserClaim<Guid>>().ToTable("UserClaims");
             builder.Entity<IdentityUserLogin<Guid>>().ToTable("UserLogins");
             builder.Entity<IdentityRoleClaim<Guid>>().ToTable("RoleClaims");
             builder.Entity<IdentityUserToken<Guid>>().ToTable("UserTokens");
